7a0c2725b9b18d74aa077153619bc1d0ccac4519
Alok sinha <89570069+AlokPng@users.noreply.github.com>
2023-06-20 22:44:08 +0530

    Add files via upload
    port scanner-it give list of open port

---
+++ network.py
@@ -0,0 +1,35 @@
+import sys
+import socket
+from datetime import datetime
+
+
+#input'
+target=input(str("Target Ip: "))
+
+
+#screen
+
+print("-"*50)
+print("Scanning Target :"+ target)
+print("Scanning started at:"+str(datetime.now()))
+
+print("-"*50)
+
+try:
+    for port in range(1,65535):
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        socket.setdefaulttimeout(0.5)
+
+        #return open port
+        result=s.connect_ex((target,port))
+        if result==0:
+            print("[*] Port {} is open".format(port))
+        s.close()
+
+except KeyboardInterrupt:
+    print("\n Exiting :(")
+    sys.exit()
+
+except socket.error:
+    print("\Host not responding")
+    sys.exit()
